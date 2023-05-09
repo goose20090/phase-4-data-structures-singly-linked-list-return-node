e86bcb29cd58c3e4d72346a2e60f6787bf2c9476
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-05-09 14:19:29 +0100

    commit tree cc1f2d9d891d21adf061549ab9c0420a9dfabaf9

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,8 +7,47 @@ class LinkedList
     @head = nil
   end
 
+  def search_with_index index
+
+    current_index = 0
+
+    current_node = self.head
+
+    until current_index == index
+      current_node = current_node.next_node
+      current_index+= 1
+    end
+
+    current_node.value
+  end
+
   def nth_from_end(n)
-    # your code here
+
+    if !self.head || n < 1
+      return nil
+    end
+
+
+    last_node = self.head
+
+    length = 1
+
+    # find index of final item
+
+    while last_node.next_node
+      last_node = last_node.next_node
+      length += 1
+    end
+
+    if n > length
+      return nil
+    end
+
+    # subtract length from n index
+    index = length - n 
+
+    search_with_index(index)
+
   end
 
 end
